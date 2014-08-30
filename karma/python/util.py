9dccf5ac6e00d5253696a97902a365352adf9325
szeke <szekely1401@gmail.com>
2014-08-29 22:46:43 -0700

    Models using schema.org

--- karma/python/util.py
+++ karma/python/util.py
@@ -2,3 +2,10 @@ def documentUrl(x):
 	"Return the original document URL from the URL in the document version"
 	i = x.find('churl')
 	return 'http://'+x[x.find('/',i+6)+1:]
+
+
+def countryUri(x):
+	"Return a URI for a country given its name."
+	import re
+	x = re.sub('[^A-Za-z0-9]+', '', getValue("Country"))
+	return x.lower()
