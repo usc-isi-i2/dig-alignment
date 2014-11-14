c06c289b4e905e59850acd24c26bbb2b45d56d91
Dipsy Kapoor <dipsykapoor@gmail.com>
2014-11-14 09:52:54 -0800

    DIG-79 @comment fixed prov, ns and dates for istr images

--- datasets/istr/offlinescripts.sh
+++ datasets/istr/offlinescripts.sh
@@ -2,7 +2,7 @@
 ########################################################
 #			Images
 ########################################################
-mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.args="--sourcetype SQL --dbtype MySQL --hostname 23.101.198.62 --username dipsy --password sqlpassword --portnumber 3306 --dbname memex_small --queryfile /Users/dipsy/github/dig-alignment/datasets/istr/images/import.sql --modelfilepath /Users/dipsy/github/dig-alignment/datasets/istr/images/ist-images-model.ttl --jsonoutputfile /Users/dipsy/github/dig-alignment/datasets/istr/images/x-ist-images-jsonld.json --contextfile /Users/dipsy/github/dig-alignment/datasets/istr/context-for-istr-datasets.json --outputfile /Users/dipsy/github/dig-alignment/datasets/istr/images/x-ist-images-rdf.n3" -Dexec.classpathScope=compile
+mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.args="--sourcetype SQL --dbtype MySQL --hostname 23.101.198.62 --username dipsy --password sqlpassword --portnumber 3306 --dbname memex_small --queryfile /Users/dipsy/github/dig-alignment/datasets/istr/images/import.sql --modelfilepath /Users/dipsy/github/dig-alignment/datasets/istr/images/ist-images-model.ttl --jsonoutputfile /Users/dipsy/github/dig-alignment/datasets/istr/images/ist-images-jsonld.json --contextfile /Users/dipsy/github/dig-alignment/datasets/istr/context-for-istr-datasets.json --outputfile /Users/dipsy/github/dig-alignment/datasets/istr/images/ist-images-rdf.n3" -Dexec.classpathScope=compile
 
 
 ########################################################
