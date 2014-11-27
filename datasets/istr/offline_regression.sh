3682c6874caa1967d139f20e662a393cef9059b9
jasonslepicka <jasonslepicka@gmail.com>
2014-11-26 22:09:47 -0800

    Add JSON root to regression tests as needed

--- datasets/istr/offline_regression.sh
+++ datasets/istr/offline_regression.sh
@@ -34,30 +34,35 @@ function compare_outputs {
 function run_offline_with_defaults_images {
 	BASE_DIR="${DIG_ALIGNMENT_HOME}/datasets/istr/images"
 	INPUT_FILE="${BASE_DIR}/images-sample.json"
-	run_offline_with_defaults "Images" "$INPUT_FILE" "${BASE_DIR}/ist-images-model.ttl" "$BASE_DIR/ist-images-sample-jsonld.json" "$BASE_DIR/ist-images-sample-rdf.n3"
+	run_offline_with_defaults "Images" "$INPUT_FILE" "${BASE_DIR}/ist-images-model.ttl" "$BASE_DIR/ist-images-sample-jsonld.json" "$BASE_DIR/ist-images-sample-rdf.n3" "http://schema.org/WebPage1"
 }
 
 function run_offline_with_defaults_ads {
 	BASE_DIR="${DIG_ALIGNMENT_HOME}/datasets/istr/ads"
-	INPUT_FILE="${BASE_DIR}/ads-sample.json"
-	run_offline_with_defaults "Ads" "$INPUT_FILE" "${BASE_DIR}/model-${1}/ads-${1}-model.ttl" "$BASE_DIR/model-${1}/ads-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/ads-${1}-sample-rdf.n3"
+	run_offline_with_defaults "Ads" "${BASE_DIR}/${2}" "${BASE_DIR}/model-${1}/ads-${1}-model.ttl" "$BASE_DIR/model-${1}/ads-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/ads-${1}-sample-rdf.n3" "http://schema.org/WebPage1"
 }
 
 function run_offline_with_defaults_stanford_extraction {
 	BASE_DIR="${DIG_ALIGNMENT_HOME}/datasets/istr/stanford-extractions"
 	INPUT_FILE="${BASE_DIR}/stanford-features-sample.json"
-	run_offline_with_defaults "Stanford-Extractions" "$INPUT_FILE" "${BASE_DIR}/model-${1}/stanford-${1}-model.ttl" "$BASE_DIR/model-${1}/stanford-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/stanford-${1}-sample-rdf.n3"
+	run_offline_with_defaults "Stanford-Extractions" "$INPUT_FILE" "${BASE_DIR}/model-${1}/stanford-${1}-model.ttl" "$BASE_DIR/model-${1}/stanford-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/stanford-${1}-sample-rdf.n3" "http://schema.org/WebPage1"
 }
 
 function run_offline_with_defaults_ads_attributes {
 	BASE_DIR="${DIG_ALIGNMENT_HOME}/datasets/istr/ads-attributes"
 	INPUT_FILE="${DIG_ALIGNMENT_HOME}/datasets/istr/ads-attributes/add-attributes-sample-100.json"
-	run_offline_with_defaults "Ads-Attributes" "$INPUT_FILE" "${BASE_DIR}/model-${1}/ads-attributes-${1}-model.ttl" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-rdf.n3"
+	run_offline_with_defaults "Ads-Attributes" "$INPUT_FILE" "${BASE_DIR}/model-${1}/ads-attributes-${1}-model.ttl" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-rdf.n3" "http://schema.org/WebPage1"
+}
+
+function run_offline_with_defaults_ads_attributes_features {
+	BASE_DIR="${DIG_ALIGNMENT_HOME}/datasets/istr/ads-attributes"
+	INPUT_FILE="${DIG_ALIGNMENT_HOME}/datasets/istr/ads-attributes/add-attributes-sample-100.json"
+	run_offline_with_defaults "Ads-Attributes" "$INPUT_FILE" "${BASE_DIR}/model-${1}/ads-attributes-${1}-model.ttl" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-jsonld.json" "$BASE_DIR/model-${1}/ads-attributes-${1}-sample-rdf.n3" "http://memexproxy.com/ontology/Feature1"
 }
 
 function run_offline_with_defaults {
 
-	run_offline "JSON" "${1}" "DEFAULT_TEST" "${2}" "${3}" "${4}" "$CONTEXT_URL" "${5}"
+	run_offline "JSON" "${1}" "DEFAULT_TEST" "${2}" "${3}" "${4}" "$CONTEXT_URL" "${5}" "${6}"
 }
 function run_offline {
 mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.args="\
@@ -68,7 +73,8 @@ mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.ar
 --modelfilepath ${5} \
 --jsonoutputfile ${6} \
 --contexturl ${7} \
---outputfile ${8}" \
+--outputfile ${8} \
+--root ${9}" \
 >> karma.out 2>> karma.err
 RETURN=$?
 if [ "${RETURN}" -ne 0 ]
@@ -88,31 +94,31 @@ compare_outputs_images
 ########################################################
 #			Ads
 ########################################################
-run_offline_with_defaults_ads "main"
+run_offline_with_defaults_ads "main" "ads-sample.json"
 compare_outputs_ads "main"
 
 #Ads phone
-run_offline_with_defaults_ads "phone"
+run_offline_with_defaults_ads "phone" "ads-sample.json"
 compare_outputs_ads "phone"
 
 #Ads address
-run_offline_with_defaults_ads "address"
+run_offline_with_defaults_ads "address" "model-address/ads-addresses-sample.json"
 compare_outputs_ads "address"
 
 #Ads age
-run_offline_with_defaults_ads "age"
+run_offline_with_defaults_ads "age" "ads-sample.json"
 compare_outputs_ads "age"
 
 #Ads website
-run_offline_with_defaults_ads "website"
+run_offline_with_defaults_ads "website" "ads-sample.json"
 compare_outputs_ads "website"
 
 #Ads gender
-run_offline_with_defaults_ads "gender"
+run_offline_with_defaults_ads "gender" "ads-sample.json"
 compare_outputs_ads "gender"
 
 #Ads email
-run_offline_with_defaults_ads "email"
+run_offline_with_defaults_ads "email" "ads-sample.json"
 compare_outputs_ads "email"
 
 
@@ -122,10 +128,10 @@ compare_outputs_ads "email"
 run_offline_with_defaults_ads_attributes "main"
 compare_outputs_ads_attributes "main"
 
-run_offline_with_defaults_ads_attributes "rate"
+run_offline_with_defaults_ads_attributes_features "rate"
 compare_outputs_ads_attributes "rate"
 
-run_offline_with_defaults_ads_attributes "phone"
+run_offline_with_defaults_ads_attributes_features "phone"
 compare_outputs_ads_attributes "phone"
 
 
