47c83f9baffd96bc184f4752605c805b4d4e0e42
Dipsy Kapoor <dipsykapoor@gmail.com>
2015-01-05 12:55:02 -0800

    Fix state names: assume US as country if no country is present

--- karma/python/ist-feature.py
+++ karma/python/ist-feature.py
@@ -635,6 +635,13 @@ def clean_state(state, country):
     state = clean_location(state)
     if country:
         state = standardize_state_name(country, state)
+    else:
+        state_us = standardize_state_name("US", state)
+        if len(state_us) > 0:
+            state = state_us
+        else:
+            if len(state) == 2:
+                state = state.upper() #upper case state code
 
     return state
 
