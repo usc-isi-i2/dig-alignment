7cb60b0856503bd5ee4ebf182d961325d2339743
szeke <szekely1401@gmail.com>
2015-04-14 19:27:59 -0400

    basic structure

--- karma/python/atf-scripts.py
+++ karma/python/atf-scripts.py
@@ -1,8 +1,14 @@
 # Scripts for modeling ATF data.
 
-def atf_article_uri(url):
-	return get_url_hash(url)
+def atf_article_uri(url, post_id):
+	return get_url_hash(url)+"/"+post_id
 
 
 def atf_date_created(date):
+	"""Put the date in ISO format"""
 	return date
+
+
+def signature_clean(text):
+	"""Strip HTML"""
+	return text
