1f87a4cccc828daaf7c15201a38ab551990d23d5
Andrew Philpot <andrew.philpot@gmail.com>
2015-06-16 16:12:30 -0700

    should be moved to hg2json dir

--- datasets/atf/bin/prep_hg2json_files.sh
+++ datasets/atf/bin/prep_hg2json_files.sh
@@ -5,18 +5,24 @@ DIR=/Users/philpot/Documents/project/dig-alignment/datasets/atf/hg2json
 TEMPFILE=$(mktemp -t "$0")
 touch ${TEMPFILE}
 
-cat ${DIR}/smf/smf_abraxasgacelesox.onion.jl | sed -e 's#^{#{"originalfile": "smf_abraxasgacelesox.onion.jl",#' >> ${TEMPFILE}
-cat ${DIR}/z34/phpbb_z34uj4opd3tejafn.onion.jl | sed -e 's#^{#{"originalfile": "phpbb_z34uj4opd3tejafn.onion.jl",#' >> ${TEMPFILE}
 
+cat ${DIR}/smf/smf_abraxasgacelesox.onion.json | sed -e 's#^{#{"originalfile": "smf_abraxasgacelesox.onion.json",#' >> ${TEMPFILE}
 # add comma after every line
-sed -i ".bak1" "s/$/,/g" ${TEMPFILE}
-
-# now delete last comma
-sed -i '.bak2' '$s/,$//' ${TEMPFILE}
-
+sed -i ".bak1" 's/$/,/g' ${TEMPFILE}
+# now delete last comma	
+sed -i ".bak2" '$s/,$//' ${TEMPFILE}
 # now add a [ ] around it
+{ echo "[" ; cat ${TEMPFILE} ; echo "]" ; } > ${DIR}/labeled_smf_abraxasgacelesox.onion.json
+rm ${TEMPFILE}
+rm ${TEMPFILE}.bak*
 
-{ echo "[" ; cat ${TEMPFILE} ; echo "]" ; } > ${DIR}/labeled_hg2json_all.json
-
+touch ${TEMPFILE}
+cat ${DIR}/z34/phpbb_z34uj4opd3tejafn.onion.json | sed -e 's#^{#{"originalfile": "phpbb_z34uj4opd3tejafn.onion.json",#' >> ${TEMPFILE}
+# add comma after every line
+sed -i ".bak1" 's/$/,/g' ${TEMPFILE}
+# now delete last comma	
+sed -i ".bak2" '$s/,$//' ${TEMPFILE}
+# now add a [ ] around it
+{ echo "[" ; cat ${TEMPFILE} ; echo "]" ; } > ${DIR}/labeled_phpbb_z34uj4opd3tejafn.onion.json
 rm ${TEMPFILE}
 rm ${TEMPFILE}.bak*
