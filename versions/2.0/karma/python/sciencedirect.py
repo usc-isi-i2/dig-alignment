e0eec5f1dc3fce45b705a2aaab7ad973ff37cc81
szeke <szekely1401@gmail.com>
2015-07-16 19:42:49 -0400

    Define URI for science direct based on doi

--- versions/2.0/karma/python/sciencedirect.py
+++ versions/2.0/karma/python/sciencedirect.py
@@ -9,4 +9,8 @@ def author_uri(name):
     first_name = name.split()[0]
     last_name = name.split()[1]
     return  "author/" + first_name[0].lower() + '.' + last_name.lower()
-    
+
+
+def sd_article_uri(doi):
+	"""Construct the URI for an article using the DOI"""
+	return 'article/sciencedirect/'+doi
