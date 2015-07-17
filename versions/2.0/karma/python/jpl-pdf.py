9c2495f1ffe673a76954abad2d5993dc18753455
rajagopal067 <raj.bhojan@gmail.com>
2015-07-17 16:15:52 -0400

    changed the logic for author uri

--- versions/2.0/karma/python/jpl-pdf.py
+++ versions/2.0/karma/python/jpl-pdf.py
@@ -2,10 +2,13 @@
 from datetime import datetime, date
 from time import mktime, gmtime
 
-
 def jp_author_uri(forename, surname):
-	"""Construct the URI of an author so that it will reduce correctly most of the time."""
-	return 'person/'+forename[0].lower()+"_"+surname.lower()
+ """Construct the URI of an author so that it will reduce correctly most of the time."""
+ first_word = forename.replace('.','')
+ second_word = surname.replace('.','')
+ if len(second_word) < 3:
+    return "author/" + second_word[0].lower() + '.' + first_word.lower()
+ return  "author/" + first_word[0].lower() + '.' + second_word.lower()
 
 
 def jp_author_name(forename, surname):
