dcc210eee4afbdff743d4874004cee6588dd79fe
szeke <szekely1401@gmail.com>
2015-07-27 12:40:15 -0700

    Consolidate electronics py files

--- versions/2.0/karma/python/abacusselect.py
+++
@@ -1,7 +0,0 @@
-
-# returns sha1 of manufacturer and model
-def as_getTextHash(manufacturer,model):
-    text=manufacturer+model
-    if text:
-        return hashlib.sha1(text.encode('utf-8')).hexdigest().upper()
-    return ''
--- versions/2.0/karma/python/usbid.py
+++
@@ -1,7 +0,0 @@
-
-# returns sha1 of manufacturer and model
-def us_getTextHash(manufacturer,model):
-    text=manufacturer+model
-    if text:
-        return hashlib.sha1(text.encode('utf-8')).hexdigest().upper()
-    return ''
