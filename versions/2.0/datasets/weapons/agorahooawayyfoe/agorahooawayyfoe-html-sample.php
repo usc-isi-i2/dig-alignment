1c9aa47ba95cc37d71c5f7747ca8da485f110c4f
Brian Amanatullah <bamanatullah@inferlink.com>
2015-08-26 10:41:38 -0700

    Adding agorahooawayyfoe extraction rules and sample

---
+++ versions/2.0/datasets/weapons/agorahooawayyfoe/agorahooawayyfoe-html-sample.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Silent But Deadly: More Homemade Silencers</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="/css/FontAwesome.css" type="text/css">
+        <link rel="stylesheet" href="/css/main.css" type="text/css">
+		
+  
+
+
+        <style type="text/css">
+        
+
+        .market-wide-message {
+            background-color: #abdcfb; border: 1px solid black; clear: both; display: block; margin-bottom: 15px; padding: 10px;
+        }
+
+        </style>
+	</head>
+	<body>
+
+    
+    
+    <div id="topmost">
+        <table style="width: 100%; height: 100%;" cellpadding="0" cellspacing="0">
+            <tr>
+                <td id="smalllogo-td">Ag</td>
+                <td style="width: 20px; overflow: hidden;">&nbsp;</td>
+                <td id="topmost-middlecontent">
+                    <div id="page-heading">Agora <span style="color: #ccc;">Beta</span></div>
+                    <div id="topmenu-container">
+                        
+                            <a class="topmenu-element" href="/">Listings</a>
+                            <a class="topmenu-element" href="/profile">Profile</a>
+                            <a class="topmenu-element" href="/wallet">Wallet</a>
+                            
+                                
+                                    <a class="topmenu-element" href="/orders">Orders</a>
+                                
+                            
+                            
+                            
+                            <a class="topmenu-element" href="http://lacbzxobeprssrfx.onion" target="_blank">Forums</a>
+                            
+                            
+
+                        
+                        
+                        <a class="topmenu-element" href="/info">Info/Help</a>
+                    </div>
+                </td>
+                <td style="text-align: right; padding-right: 40px;">
+                    <div id="login-info-bar">
+                        
+                            Welcome
+                            
+                            zeus546
+                            :: <a href="/wallet">Wallet 0.00000000 BTC</a>
+                            
+                            :: <i class="fa fa-btc"></i> 230.53 USD
+                        
+                    </div>
+                    <div id="login-buttons-bar">
+                        
+
+                            
+
+                            
+                                <div class="button-grey">
+                            
+                                <a href="/messagelist"><i class="fa fa-comments"></i>&nbsp;&nbsp;0 new messages</a>
+                            </div>
+
+                            <div class="button-red"><a href="/logout"><i class="fa fa-power-off"></i>&nbsp;&nbsp;Logout</a></div>
+                        
+                    </div>
+                </td>
+
+            </tr>
+        </table>
+    </div>
+    <div style="width: 100%; height: 1px; background-color: #d9d9d9; overflow: hidden;">&nbsp;</div>
+    <div style="width: 100%; height: 1px; background-color: #FFFFFF; overflow: hidden;">&nbsp;</div>
+    <div id="top-navigation">
+
+        
+    <div class="topnav-element"><a href="/cat/PfjGSF75ez">Info/eBooks</a></div><div class="topnav-divider-element">&gt;</div><div class="topnav-element"><a href="/cat/kbbm5mEK9l">Other</a></div><div class="topnav-divider-element">&gt;</div>
+
+
+
+
+        <div style="clear: both;"></div>
+    </div>
+
+    <div id="aftertopnav-divider-1"></div>
+    <div id="aftertopnav-divider-2"></div>
+
+    <div id="middlestuff" style="margin: 0 20px;">
+        
+            <table class="mainpagedivider" cellpadding="0" style="border: 0; padding: 0; margin: 0;"><tr><td style="width: 160px;" class="mainpagedividertd">
+            <div id="leftmenu">
+
+                
+
+                
+    
+
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/PfjGSF75ez">Info/eBooks</a></div>
+    <div class="leftmenu-subelements">
+
+
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/GfgmcWsMFb">Aliens/UFOs<span class="extra-caption"> (40+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/Wdq1ADc51U">Anonymity<span class="extra-caption"> (200+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/giSyWeCyx4">Doomsday<span class="extra-caption"> (90+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/JAmvyj8iBs">Drugs<span class="extra-caption"> (300+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/mYDYB1iw03">Economy<span class="extra-caption"> (100+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/mglUzajVoX">IT<span class="extra-caption"> (200+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/XydNoXSNwb">Making money<span class="extra-caption"> (600+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/wydUs5lG69">Philosophy<span class="extra-caption"> (80+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/LaFGtBP71g">Politics<span class="extra-caption"> (70+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/fR1ap1bk9X">Psychology<span class="extra-caption"> (200+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/qbnTjNNGP9">Relationships/Sex<span class="extra-caption"> (100+)</span></a></div>
+
+    
+    <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/i5yVJFraA8">Science<span class="extra-caption"> (200+)</span></a></div>
+
+    
+    <div class="leftmenu-element leftmenu-element-selected"><div class="drawer"></div><a href="/cat/kbbm5mEK9l">Other<span class="extra-caption"> (1600+)</span></a></div>
+
+
+
+    </div> 
+
+    
+
+    <div style="padding-top: 20px; padding-bottom: 5px; color: #aaa;">Main menu:</div>
+
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/yzlwMgTYoP">Chemicals<span class="extra-caption"> (10+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/iagjMlD0Cr">Counterfeits<span class="extra-caption"> (1100+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/sqxrhAitt4">Data<span class="extra-caption"> (1300+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/VK5iK8xY0i">Drug paraphernalia<span class="extra-caption"> (400+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/vheN9d2mMr">Drugs<span class="extra-caption"> (16800+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/nL0oges5ZX">Electronics<span class="extra-caption"> (200+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/uJM18V1Fyy">Forgeries<span class="extra-caption"> (300+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/PfjGSF75ez">Info/eBooks<span class="extra-caption"> (4100+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/EKbrBV7frb">Jewelry<span class="extra-caption"> (200+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/sYqAB5BE46">Services<span class="extra-caption"> (1000+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/tFCxKyZhJm">Tobacco<span class="extra-caption"> (100+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/eD8l7PYEK3">Weapons<span class="extra-caption"> (100+)</span></a></div>
+    
+        
+        <div class="leftmenu-element "><div class="drawer"></div><a href="/cat/evljk7AG11">Other<span class="extra-caption"> (400+)</span></a></div>
+    
+
+
+
+            </div> </td><td class="mainpagedividertd">
+        
+
+        <div style="float: left;">
+            
+                
+                
+                    
+                
+
+                <span class="market-wide-message">We have received reports of users receiving private messages containing a link to an exploit which can be used to hack accounts. We are working on a solution, but in the mean time please turn off &#74;avaScript when you browse the market.</span>
+
+
+
+
+                
+
+            
+            
+
+
+
+
+<div id="single-product" class="nofirstmargin">
+
+
+    
+
+    
+
+
+    <h1>Silent But Deadly: More Homemade Silencers</h1>
+    <div style="text-align: left;">0.00390404 BTC</div>
+    <div style="clear: both;"></div>
+    
+        <img style="display: block; float: right; padding: 0 0 20px 20px; max-width: 400px;" src="/liabilities/p/jecJSNUC0a.jpg" alt=""/>
+    
+    Despite what the Brady bunch might say, silencers serve some very civilized functions and contribute to a saner, quieter way of life, which is why a lot of folks like them. After reading The Hayduke Silencer Book, many readers shared their own ideas for simple designs you can build at home, legally. Here is the cream of the crop.<br/><br/>----------------------------------------------------------<br/><br/>This ebook is in .pdf format
+    <br/>
+    Brought to you by:<br/><a class="gen-user-link"  href="/vendor/Thrills#"/>Thrills</a><span class="gen-user-ratings"> <i class="fa fa-bar-chart-o"></i>&nbsp;4.878/5, 3000~4000 deals</span>
+    <div class="product-page-ships">
+    <i class="fa fa-truck"></i> From: Torland<br/>
+    
+    </div>
+    <div class="product-page-price">0.00390404 BTC</div>
+    <div style="clear: both;"></div>
+    <div style="text-align: right;">
+        
+            <div class="button-red"><a href="/p/jecJSNUC0a/buy">
+                <i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;Buy...</a></div>
+        
+
+    </div>
+
+    
+<div class="embedded-feedback-list">
+    <h4 style="margin: 10px 0 5px 0;">Feedbacks:</h4>
+    No feedbacks found.
+</div>
+
+</div>
+
+        </div>
+        
+            </td></tr></table>
+        
+    </div>
+    <div style="clear: both;"></div>
+
+
+   
+
+	</body>
+</html>
